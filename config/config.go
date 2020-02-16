911a4101da063a024b0bb645912d043d71d469f2
zeromsi <wwww.zeromsi2@gmail.com>
2020-02-16 12:31:22 +0600

    rename module name

--- config/config.go
+++ config/config.go
@@ -1,19 +1,6 @@
 package config
 
-import (
-	"github.com/joho/godotenv"
-	"os"
-)
 var Username string
 var Password string
 var LokiUrl string
 var LokiWSUrl string
-func InitEnvironmentVariables() {
-
-	 godotenv.Load()
-
-	Username = os.Getenv("USER_NAME")
-	Password = os.Getenv("PASSWORD")
-	LokiUrl = os.Getenv("LOKI_URL")
-	LokiWSUrl = os.Getenv("LOKIWS_URL")
-}
